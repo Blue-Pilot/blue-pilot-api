a35958c7289784e340c5d219ba48ce4dcff1f749
Zach Gyurko <classicpizzazach@gmail.com>
2024-04-06 17:34:50 -0400

    completed 'Rating Tests'

---
+++ tests/Blue-Pilot.Domain.Tests/RatingTests.cs
@@ -0,0 +1,31 @@
+namespace Blue_Pilot.Domain.Tests;
+
+using Blue.Pilot.Domain.Catalog;
+using Blue_Pilot.Domain;
+
+[TestClass]
+public class RatingTests
+{
+    [TestMethod]
+
+    [ExpectedException(typeof(ArgumentException))]
+    public void Cannot_Create_Rating_With_Invalid_Stars() {
+        // Arrange
+        var rating = new Rating (0, "Mike", "Great fit!");
+    }
+    
+    public void Can_Create_New_Rating() {
+
+    // Arrange
+    var rating = new Rating (1, "Mike", "Great fit!");
+
+    // Act (empty)
+
+    // Assert
+    Assert.AreEqual (1, rating.Stars);
+    Assert.AreEqual ("Mike", rating.UserName);
+    Assert.AreEqual ("Great fit!", rating.Review);
+}
+
+
+}
