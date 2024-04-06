0c433dc3a76fdb4d28f8079be1676517054201e9
Zach Gyurko <classicpizzazach@gmail.com>
2024-04-06 17:36:02 -0400

    minor formatting changes

--- tests/Blue-Pilot.Domain.Tests/RatingTests.cs
+++ tests/Blue-Pilot.Domain.Tests/RatingTests.cs
@@ -10,21 +10,22 @@ public class RatingTests
 
     [ExpectedException(typeof(ArgumentException))]
     public void Cannot_Create_Rating_With_Invalid_Stars() {
+        
         // Arrange
         var rating = new Rating (0, "Mike", "Great fit!");
     }
-    
+
     public void Can_Create_New_Rating() {
 
-    // Arrange
-    var rating = new Rating (1, "Mike", "Great fit!");
+        // Arrange
+        var rating = new Rating (1, "Mike", "Great fit!");
 
-    // Act (empty)
+        // Act (empty)
 
-    // Assert
-    Assert.AreEqual (1, rating.Stars);
-    Assert.AreEqual ("Mike", rating.UserName);
-    Assert.AreEqual ("Great fit!", rating.Review);
+        // Assert
+        Assert.AreEqual (1, rating.Stars);
+        Assert.AreEqual ("Mike", rating.UserName);
+        Assert.AreEqual ("Great fit!", rating.Review);
 }
 
 
